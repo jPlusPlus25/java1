96130d84edba849e5affcde08d4349631d772116
arshita <arshitapatel077@gmail.com>
2025-02-14 16:00:20 +0530

    using continue statement

---
+++ continue1.java
@@ -0,0 +1,17 @@
+//how to use coninue
+public class continue1 {
+        public static void main(String []args){
+            for(int i=1;i<=100;i++){
+                if(i==49){
+                    continue;
+                }
+                if(i==50){
+                    break;
+                }
+                System.out.println(i);
+                System.out.println("end");
+            }
+        }
+    }
+
+
