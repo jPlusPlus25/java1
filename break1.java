a2e41890dce4ce3ec7210bd5f9804a8d3a402f45
arshita <arshitapatel077@gmail.com>
2025-02-14 15:59:44 +0530

    using break statement

---
+++ break1.java
@@ -0,0 +1,11 @@
+//how to use break
+public class break1 {
+    public static void main(String []args){
+        for(int i=1;i<=100;i++){
+            if(i==50){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
