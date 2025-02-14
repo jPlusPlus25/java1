75cdb854a63d194eaec0c64e24cf13a835327e26
arshita <arshitapatel077@gmail.com>
2025-02-14 15:59:07 +0530

    using for each loop

---
+++ arshita.java
@@ -0,0 +1,13 @@
+public class arshita {
+    public static void main(String args[]){
+        System.out.println("ars");
+        int a[]={1,2,3,4,5};
+        int k=0;
+        for(int r:a){
+            k=k+r;
+            System.out.println(k);
+
+        }
+        System.out.println(k);
+    }
+}
