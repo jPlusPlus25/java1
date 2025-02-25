64fdc017cbb56e50634a5c39eb3c3a365aac5b49
arshita <arshitapatel077@gmail.com>
2025-02-25 22:29:07 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,21 @@
+public class Demo {
+    
+        public static void main(String[] args) {
+            Sa sa1=new Sa();
+            sa1.setAcc_Id(101);
+            sa1.setAcc_Name("Rahul");
+            sa1.setAcc_Email("rg@gmail.com");
+            sa1.setAcc_Bal(5000);
+            sa1.cal_Bal();
+    
+            Ca ca1=new Ca();
+            ca1.setAcc_Id(2001);
+            ca1.setAcc_Name("Sonia");
+            ca1.setAcc_Email("sg@gmail.com");
+            ca1.setAcc_Bal(55000);
+            ca1.cal_Bal();
+        }
+    }
+
+    
+
